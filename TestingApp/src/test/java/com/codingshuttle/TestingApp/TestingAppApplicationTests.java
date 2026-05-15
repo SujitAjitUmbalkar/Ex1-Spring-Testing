430791145f63b45ffc070632e2a640710f8b6bca
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-05-15 21:59:11 +0530

    concept of @SpringBootTest ,@Test ,@DisplayName , @Disabled etc

--- TestingApp/src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
+++ TestingApp/src/test/java/com/codingshuttle/TestingApp/TestingAppApplicationTests.java
@@ -1,13 +1,28 @@
 package com.codingshuttle.TestingApp;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TestingAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//@SpringBootTest               // runs whole application , data gets stored in real db
+// after removing it only test methods are run
+class TestingAppApplicationTests
+{
+
+    @Test           // mark any method as test
+    @Disabled   // use to disable any method ( use to stop particular method to run )
+    void contextLoads()
+    {
+
+    }
+
+    @Test
+    @DisplayName("DisplayTestNameAsanyTest")            // renames test method name at execution
+    void anyTest()
+    {
+
+    }
 
 }
