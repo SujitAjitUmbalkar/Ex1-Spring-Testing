0de00d4eaac8b35794b0d4d542f16b6365b6d4f6
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-05-21 12:00:53 +0530

    Test testFindByEmail_whenEmailIsPresent_thenReturnEmployee is written by using Arrange , act , assert technique

--- TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
+++ TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
@@ -1,10 +1,13 @@
 package com.codingshuttle.TestingApp.repositories;
 
+import com.codingshuttle.TestingApp.entities.Employee;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 @DataJpaTest
 //@AutoConfigureTestDatabase(
@@ -12,9 +15,35 @@ import static org.junit.jupiter.api.Assertions.*;
 //)
 class EmployeeRepositoryTest
 {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    private Employee employee;                          // 1 step
+
+    @BeforeEach                                             // 2 step
+    public  void setup()
+    {
+        employee = Employee.builder()
+//                .id(1L)       // not needed if entity is managing id's itself
+                .name("Sujit")
+                .email("jeetumbalkar@gmail.com")
+                .salary(10000L)
+                .build();
+    }
+
     @Test
     void testFindByEmail_whenEmailIsPresent_thenReturnEmployee()
     {
+//        Arrange (given)
+        employeeRepository.save(employee);
+
+//        Act
+        List<Employee> employees = employeeRepository.findByEmail(employee.getEmail());
+
+//        Assert
+        Assertions.assertThat(employees).isNotNull();
+        Assertions.assertThat(employees).isNotEmpty();
+        Assertions.assertThat(employees.get(0).getEmail()).isEqualTo(employee.getEmail());
 
     }
 
