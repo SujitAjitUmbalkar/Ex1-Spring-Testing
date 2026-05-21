ed94fe799a5145455694e1ff2ee1bd08f775878d
Sujit Ajit Umbalkar <jeetumbalkar222@gmail.com>
2026-05-21 14:08:08 +0530

    trying to connect Docker through TestContainers , cant connect , will learn later , lets switch to H2

---
+++ TestingApp/src/test/java/com/codingshuttle/TestingApp/TestcontainersConfiguration.java
@@ -0,0 +1,22 @@
+package com.codingshuttle.TestingApp;
+
+import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestcontainersConfiguration
+{
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer<?> mysqlContainer() {
+
+        return new MySQLContainer<>(
+                DockerImageName.parse("mysql:8.0.36")
+        );
+    }
+}
--- TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
+++ TestingApp/src/test/java/com/codingshuttle/TestingApp/repositories/EmployeeRepositoryTest.java
@@ -1,14 +1,21 @@
 package com.codingshuttle.TestingApp.repositories;
 
+//import com.codingshuttle.TestingApp.TestcontainersConfiguration;
 import com.codingshuttle.TestingApp.entities.Employee;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.TestcontainersConfiguration;
+//import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
+@Testcontainers
+@Import(TestcontainersConfiguration.class)
 @DataJpaTest
 //@AutoConfigureTestDatabase(
 //        replace = AutoConfigureTestDatabase.Replace.ANY
